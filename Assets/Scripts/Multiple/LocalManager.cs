d12b5af1ee145ba72200c22cb3a9fddd8d5f58f3
shallowmonologue <1054211027@qq.com>
2021-12-21 17:29:57 +0800

    Revert "全身同步"
    This reverts commit e75fc545935f4e5b4cb3a15facf38d86f714da8e.

--- Assets/Scripts/Multiple/LocalManager.cs
+++ Assets/Scripts/Multiple/LocalManager.cs
@@ -1,29 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 using Photon.Pun;
 
 public class LocalManager : MonoBehaviour
 {
     public List<MonoBehaviour> LocalScripts;
-    public List<Renderer> TPRenderers;
     public Camera Gun_camera;
     public Camera Main_camera;
     public Camera Portal_camera;
     private PhotonView photonView;
-    public GameObject FPArms;
 
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
-        if (photonView.IsMine)
-        {
-            gameObject.AddComponent<AudioListener>();
-            return;
-        }
+        if (photonView.IsMine) return;
 
-        FPArms.SetActive(false);
         Gun_camera.enabled = false;
         Main_camera.enabled = false;
         Portal_camera.enabled = false;
@@ -31,10 +23,6 @@ public class LocalManager : MonoBehaviour
         foreach (MonoBehaviour behaviour in LocalScripts)
         {
             behaviour.enabled = false;
-        }
-        foreach (Renderer tpRenderer in TPRenderers)
-        {
-            tpRenderer.shadowCastingMode = ShadowCastingMode.On;
         }
     }
 }

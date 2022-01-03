c5da75de6ce4a8add27b455c25a40a8b99e202ce
aTerminal <kx78z0w@outlook.com>
2022-01-03 15:30:16 +0800

    实现武器捡拾。最终版本。

--- Assets/Scripts/Weapon/WeaponManager.cs
+++ Assets/Scripts/Weapon/WeaponManager.cs
@@ -1,4 +1,6 @@
+using Scripts.Items;
 using Scripts.Weapon;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,10 @@ public class WeaponManager : MonoBehaviour
 {
     public Firearms mainWeapon;
     public Firearms secondaryWeapon;
+    public Transform worldCameraTransform;
+    public float raycastMaxDistance;
+    public LayerMask checkItemLayerMask;
+    public List<Firearms> arms = new List<Firearms>();
 
     private Firearms carriedWeapon;
     private FPCharacterControllerMovement fPCharacterControllerMovement;
@@ -14,13 +20,18 @@ public class WeaponManager : MonoBehaviour
 
     private void Start()
     {
-        carriedWeapon = mainWeapon;
         fPCharacterControllerMovement = FindObjectOfType<FPCharacterControllerMovement>();
-        fPCharacterControllerMovement.SetupAnimator(carriedWeapon.gunAnimator);
+        if (mainWeapon != null)
+        {
+            carriedWeapon = mainWeapon;
+            fPCharacterControllerMovement.SetupAnimator(carriedWeapon.gunAnimator);
+        }
     }
 
     private void Update()
     {
+        CheckItem();
+
         if (carriedWeapon == null)
         {
             return;
@@ -49,10 +60,51 @@ public class WeaponManager : MonoBehaviour
         }
     }
 
+    private void CheckItem()
+    {
+        bool tmp_IsItem = Physics.Raycast(worldCameraTransform.position, worldCameraTransform.forward, out RaycastHit tmp_RaycastHit, raycastMaxDistance, checkItemLayerMask);
+
+        if (tmp_IsItem)
+        {
+            if (Input.GetButtonDown("GetItem"))
+            {
+                bool tmp_HasItem = tmp_RaycastHit.collider.TryGetComponent(out BaseItem tmp_BaseItem);
+                if (tmp_HasItem)
+                {
+                    if (tmp_BaseItem is FirearmsItem tmp_FirearmsItem)
+                    {
+                        foreach (Firearms tmp_arm in arms)
+                        {
+                            if (tmp_FirearmsItem.armsName.CompareTo(tmp_arm.name) == 0)
+                            {
+                                switch(tmp_FirearmsItem.currentFirearmsType)
+                                {
+                                    case FirearmsItem.FirearmsType.AssaultRifle:
+                                        mainWeapon = tmp_arm;
+                                        break;
+                                    case FirearmsItem.FirearmsType.HandGun:
+                                        secondaryWeapon = tmp_arm;
+                                        break;
+                                    default:
+                                        throw new ArgumentOutOfRangeException();
+                                }
+                                SetupCarriedWeapon(tmp_arm);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private void SwapWeapon()
     {
         if (Input.GetButtonDown("SwitchMainWeapon"))
         {
+            if (mainWeapon == null)
+            {
+                return;
+            }
             if (carriedWeapon == mainWeapon)
             {
                 return ;
@@ -69,6 +121,10 @@ public class WeaponManager : MonoBehaviour
         }
         if (Input.GetButtonDown("SwitchSecondaryWeapon"))
         {
+            if (secondaryWeapon == null)
+            {
+                return;
+            }
             if (carriedWeapon == secondaryWeapon)
             {
                 return;
@@ -115,8 +171,12 @@ public class WeaponManager : MonoBehaviour
 
     private void SetupCarriedWeapon(Firearms _targetWeapon)
     {
-        carriedWeapon.gameObject.SetActive(false);
+        if (carriedWeapon)
+        {
+            carriedWeapon.gameObject.SetActive(false);
+        }
         carriedWeapon = _targetWeapon;
         carriedWeapon.gameObject.SetActive(true);
+        fPCharacterControllerMovement.SetupAnimator(carriedWeapon.gunAnimator);
     }
 }
